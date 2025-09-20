02d41e0c1e2fd097490e810f1b46a5409d73778c
Adam Adhar <adamadhar28@gmail.com>
2025-09-20 19:37:13 +0200

    + Inputs handler

--- Assets/_System/Components/PlayerInputData.cs
+++
@@ -1,7 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-public struct PlayerInputData : IComponentData
-{
-    public float2 Value;
-}
---
+++ Assets/_System/PlayerController.cs
@@ -0,0 +1,54 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameInputs _gameInputs;
+
+    private Vector2 _inputDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        if (_gameInputs == null)
+            _gameInputs = new GameInputs();
+    }
+
+    void OnEnable()
+    {
+        _gameInputs.Player.Move.performed += HandleMoveInput;
+        _gameInputs.Player.Move.canceled += HandleMoveInput;
+
+        _gameInputs.Enable();
+    }
+
+    void OnDisable()
+    {
+        _gameInputs.Player.Move.performed -= HandleMoveInput;
+        _gameInputs.Player.Move.canceled -= HandleMoveInput;
+
+        _gameInputs.Disable();
+    }
+    void Update()
+    {
+        var world = World.DefaultGameObjectInjectionWorld;
+        var entityManager = world.EntityManager;
+
+        InjectInputDirectionToECS(ref entityManager, _inputDirection);
+    }
+
+    private void HandleMoveInput(CallbackContext ctx)
+    {
+        if (ctx.canceled)
+            _inputDirection = Vector2.zero;
+
+        _inputDirection = ctx.ReadValue<Vector2>();
+    }
+
+    private void InjectInputDirectionToECS(ref EntityManager entityManager, Vector2 vec)
+    {
+        var inputEntity = entityManager.CreateEntityQuery(typeof(InputData)).GetSingletonEntity();
+        entityManager.SetComponentData(inputEntity, new InputData { Value = _inputDirection });
+    }
+}
