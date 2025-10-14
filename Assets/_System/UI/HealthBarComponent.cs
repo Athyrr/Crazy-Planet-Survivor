25d58cebf8141b379a2da2b02de878c0fc055b3b
Adam Adhar <adamadhar28@gmail.com>
2025-10-14 11:59:46 +0200

    + Player progression UI

--- Assets/_System/UI/HealthBarComponent.cs
+++ Assets/_System/UI/HealthBarComponent.cs
@@ -1,7 +1,7 @@
-using UnityEngine;
-using Unity.Entities;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Entities;
 
 public class HealthBarComponent : MonoBehaviour
 {
