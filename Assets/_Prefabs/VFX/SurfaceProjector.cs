ac6de80c7f12dba12087af4a0ea7c0a233453387
Adam Adhar <adamadhar28@gmail.com>
2025-10-15 19:09:11 +0200

    + Spell wrapped on surface shader (use heightmap later)

---
+++ Assets/_Prefabs/VFX/SurfaceProjector.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[ExecuteInEditMode] 
+public class SurfaceProjector : MonoBehaviour
+{
+    [Tooltip("Planet trasnform.")]
+    public Transform PlanetTransform;
+
+    [Tooltip("Planet radius.")]
+    public float PlanetRadius = 50f;
+
+    private Material _materialInstance;
+
+    void Start()
+    {
+        var renderer = GetComponent<Renderer>();
+        if (renderer == null)
+        {
+            Debug.LogError("Renderer not found !");
+            return;
+        }
+
+        _materialInstance = renderer.material;
+
+        UpdateShaderProperties();
+    }
+
+    private void OnValidate()
+    {
+        Start();
+    }
+
+    void Update()
+    {
+        if (_materialInstance != null && PlanetTransform != null)
+        {
+            UpdateShaderProperties();
+        }
+    }
+
+    private void UpdateShaderProperties()
+    {
+        _materialInstance.SetVector("_PlanetCenter", PlanetTransform.position);
+        _materialInstance.SetFloat("_PlanetRadius", PlanetRadius);
+    }
+}
