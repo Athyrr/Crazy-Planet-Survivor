d02ede5fb188847e85d8b4072184101aa5706fec
Adam Adhar <adamadhar28@gmail.com>
2025-10-24 03:03:41 +0200

    + Update Planet Utils : movement using heightmap

--- Assets/_System/ECS/Components/Planet/PlanetData.cs
+++ Assets/_System/ECS/Components/Planet/PlanetData.cs
@@ -1,7 +1,24 @@
+using Unity.Collections;
 using Unity.Entities;
 
 public struct PlanetData : IComponentData
 {
     public Entity Prefab;
+
+    [ReadOnly]
+    public BlobAssetReference<PlanetHeightMapBlob> HeightDataReference;
+
+    // UV resultion
+    public int FaceResolution;
+
+    // Height magnitude
+    public float MaxHeight;
+
+    // Water lvl
     public float Radius;
+}
+
+public struct PlanetHeightMapBlob
+{
+    public BlobArray<float> Heights;
 }
