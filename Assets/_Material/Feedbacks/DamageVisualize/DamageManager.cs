fd59b51ff5b1b1f3b2584d4ab16ec4be4456e632
hyverno <solo.hyverno@gmail.com>
2026-01-30 23:53:29 +0100

    clean script

--- Assets/_Material/Feedbacks/DamageVisualize/DamageManager.cs
+++ Assets/_Material/Feedbacks/DamageVisualize/DamageManager.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+using EasyButtons;
 using Random = UnityEngine.Random;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 [ExecuteAlways]
 public class DamageManager : MonoBehaviour
@@ -21,23 +17,10 @@ public class DamageManager : MonoBehaviour
     [SerializeField] public ComputeShader computeShader;
     [SerializeField] public Material displayMaterial;
     [SerializeField] public Mesh quadMesh;
-
-    [SerializeField] [Tooltip("vraiment pas fonctionnel merci de ne jamis l'utiliser")]
-    private bool _activeEditorDebug = false;
     
     private ComputeBuffer _damageBuffer;
     private List<DamageData> _activeDamages = new List<DamageData>();
     private const int _maxNumbers = 1000;
-
-    // Utilisation d'un flag pour éviter les fuites de mémoire au changement de script
-    private void OnEnable() {
-        InitBuffer();
-        #if UNITY_EDITOR
-        // Force l'update même si la scène ne bouge pas
-        if (_activeEditorDebug)
-            EditorApplication.update += EditorUpdate;
-        #endif
-    }
 
     private void InitBuffer() {
         if (_damageBuffer == null) {
@@ -66,16 +49,6 @@ public class DamageManager : MonoBehaviour
     private float GetCurrentTime() {
         return Application.isPlaying ? Time.time : (float)Time.realtimeSinceStartup;
     }
-
-    #if UNITY_EDITOR
-    void EditorUpdate() {
-        if (!Application.isPlaying) {
-            // Demande à Unity de redessiner l'interface et de déclencher Update() (askip)
-            EditorApplication.QueuePlayerLoopUpdate();
-            SceneView.RepaintAll();
-        }
-    }
-    #endif
     
     public void Start()
     {
@@ -97,26 +70,17 @@ public class DamageManager : MonoBehaviour
 
         float currentTime = GetCurrentTime();
 
-        // 1. Mise à jour via Compute Shader
         int kernel = computeShader.FindKernel("UpdateNumbers");
         computeShader.SetBuffer(kernel, "_DamageBuffer", _damageBuffer);
         computeShader.SetFloat("_Time", currentTime);
         computeShader.Dispatch(kernel, Mathf.CeilToInt(_maxNumbers / 64f), 1, 1);
 
-        // 2. Configuration du matériel
         displayMaterial.SetBuffer("_DamageBuffer", _damageBuffer);
         displayMaterial.SetFloat("_CurrentTime", currentTime);
         
-        // 3. Rendu (Fonctionne en Editor et en Game)
+        // render part
         Graphics.DrawMeshInstancedProcedural(quadMesh, 0, displayMaterial, 
             new Bounds(Vector3.zero, Vector3.one * 1000), _activeDamages.Count);
-    }
-
-    private void OnDisable() {
-        ReleaseBuffer();
-        #if UNITY_EDITOR
-        EditorApplication.update -= EditorUpdate;
-        #endif
     }
 
     private void OnDestroy() {
@@ -130,7 +94,7 @@ public class DamageManager : MonoBehaviour
         }
     }
     
-    // [Butto]
+    [Button]
     void TestDamage() {
         AddDamage(Random.Range(10, 999), Vector3.zero);
     }
