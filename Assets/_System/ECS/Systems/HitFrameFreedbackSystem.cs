6807a377ce07461fdd43cbca88d761fb67e3d447
Athyrr <adamadhar28@gmail.com>
2026-03-02 11:29:30 +0100

    + Hit frame

--- Assets/_System/ECS/Systems/HitFrameFreedbackSystem.cs
+++ Assets/_System/ECS/Systems/HitFrameFreedbackSystem.cs
@@ -1,17 +1,18 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Rendering;
+using Unity.Collections;
 
 [UpdateInGroup(typeof(PresentationSystemGroup))]
 [BurstCompile]
 public partial struct HitFrameFreedbackSystem : ISystem
 {
-    //private const string MATERIAL_PROP_NAME = "_PowerHit";
-    //private const float MATERIAL_PROP_MIN = 0;
-    //private const float MATERIAL_PROP_MAX = 1.5f;
-    //private const float FLASH_DURATION = 0.15f;
+    private const string MaterialPropName = "_PowerHit";
+    private const float MATERIAL_PROP_MIN = 0;
+    private const float MATERIAL_PROP_MAX = 2f;
+    private const float FLASH_DURATION = 1.0f; // Duration of the flash effect in seconds
 
-    //[MaterialProperty(MATERIAL_PROP_NAME)]
+    [MaterialProperty(MaterialPropName)]
     public struct HitFrameColor : IComponentData, IEnableableComponent
     {
         public float Value;
@@ -22,63 +23,66 @@ public partial struct HitFrameFreedbackSystem : ISystem
         public Entity TargetEntity;
     }
 
-    //[BurstCompile]
-    //public void OnCreate(ref SystemState state)
-    //{
-    //    state.RequireForUpdate<BeginPresentationEntityCommandBufferSystem.Singleton>();
-    //}
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<BeginPresentationEntityCommandBufferSystem.Singleton>();
+    }
 
-    //[BurstCompile]
-    //public void OnUpdate(ref SystemState state)
-    //{
-    //    var ecbSingleton = SystemAPI.GetSingleton<BeginPresentationEntityCommandBufferSystem.Singleton>();
-    //    var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var ecbSingleton = SystemAPI.GetSingleton<BeginPresentationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
 
-    //    float deltaTime = SystemAPI.Time.DeltaTime;
+        float deltaTime = SystemAPI.Time.DeltaTime;
 
-    //    foreach (var (request, requestEntity) in SystemAPI.Query<RefRO<HitFrameColorRequest>>().WithEntityAccess())
-    //    {
-    //        Entity target = request.ValueRO.TargetEntity;
+        // 1. Gestion des requêtes (Nouvel impact)
+        foreach (var (request, requestEntity) in SystemAPI.Query<RefRO<HitFrameColorRequest>>().WithEntityAccess())
+        {
+            Entity target = request.ValueRO.TargetEntity;
 
-    //        if (state.EntityManager.Exists(target))
-    //        {
-    //            if (state.EntityManager.HasBuffer<LinkedEntityGroup>(target))
-    //            {
-    //                var children = state.EntityManager.GetBuffer<LinkedEntityGroup>(target);
-    //                foreach (var child in children)
-    //                {
-    //                    if (state.EntityManager.HasComponent<MaterialMeshInfo>(child.Value))
-    //                    {
-    //                        ApplyFlash(ecb, child.Value);
-    //                    }
-    //                }
-    //            }
-    //            else if (state.EntityManager.HasComponent<MaterialMeshInfo>(target))
-    //            {
-    //                ApplyFlash(ecb, target);
-    //            }
-    //        }
+            if (state.EntityManager.Exists(target))
+            {
+                if (state.EntityManager.HasBuffer<LinkedEntityGroup>(target))
+                {
+                    var children = state.EntityManager.GetBuffer<LinkedEntityGroup>(target);
+                    foreach (var child in children)
+                    {
+                        if (state.EntityManager.HasComponent<MaterialMeshInfo>(child.Value) &&
+                            state.EntityManager.HasComponent<HitFrameColor>(child.Value))
+                        {
+                            ApplyFlash(ecb, child.Value);
+                        }
+                    }
+                }
 
-    //        ecb.DestroyEntity(requestEntity);
-    //    }
+                else if (state.EntityManager.HasComponent<MaterialMeshInfo>(target) &&
+                         state.EntityManager.HasComponent<HitFrameColor>(target))
+                {
+                    ApplyFlash(ecb, target);
+                }
+            }
 
-    //    // Color lerp
-    //    foreach (var (hitColor, entity) in SystemAPI.Query<RefRW<HitFrameColor>>().WithEntityAccess())
-    //    {
-    //        float decreasing = (MATERIAL_PROP_MAX / FLASH_DURATION) * deltaTime;
-    //        hitColor.ValueRW.Value -= decreasing;
+            ecb.DestroyEntity(requestEntity);
+        }
 
-    //        if (hitColor.ValueRW.Value <= MATERIAL_PROP_MIN)
-    //        {
-    //            hitColor.ValueRW.Value = MATERIAL_PROP_MIN;
-    //            ecb.SetComponentEnabled<HitFrameColor>(entity, false);
-    //        }
-    //    }
-    //}
+        foreach (var (hitColor, entity) in SystemAPI.Query<RefRW<HitFrameColor>>().WithEntityAccess())
+        {
+            float decreasing = (MATERIAL_PROP_MAX / FLASH_DURATION) * deltaTime;
+            hitColor.ValueRW.Value -= decreasing;
 
-    //private static void ApplyFlash(EntityCommandBuffer ecb, Entity target)
-    //{
-    //    ecb.SetComponent(target, new HitFrameColor { Value = MATERIAL_PROP_MAX });
-    //    ecb.SetComponentEnabled<HitFrameColor>(target, true);
-    //}
+            if (hitColor.ValueRW.Value <= MATERIAL_PROP_MIN)
+            {
+                hitColor.ValueRW.Value = MATERIAL_PROP_MIN;
+                ecb.SetComponentEnabled<HitFrameColor>(entity, false);
+            }
+        }
+    }
+
+    private static void ApplyFlash(EntityCommandBuffer ecb, Entity target)
+    {
+        ecb.SetComponentEnabled<HitFrameColor>(target, true);
+        ecb.SetComponent(target, new HitFrameColor { Value = MATERIAL_PROP_MAX });
+    }
 }
