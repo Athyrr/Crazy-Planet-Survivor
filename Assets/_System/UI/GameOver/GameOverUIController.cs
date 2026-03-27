eff57242883cdfce97e7772ef95498a9085fe64c
hyverno <solo.hyverno@gmail.com>
2026-03-27 13:16:58 +0100

    inject reel ressource from run

--- Assets/_System/UI/GameOver/GameOverUIController.cs
+++ Assets/_System/UI/GameOver/GameOverUIController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using _System.ECS.Authorings.Ressources;
 using UnityEngine;
 using TMPro;
 
@@ -14,39 +15,39 @@ public class GameOverUIController : MonoBehaviour
     public SummaryView RunSummaryView;
 
 
-    public void OpenView(EEndRunState endState)
+    public void OpenView(EEndRunState endState, PlayerRessources ressources)
     {
         gameObject.SetActive(true);
         GameOverView.SetActive(true);
         RunSummaryView.gameObject.SetActive(false);
 
-        SetupText(endState);
+        StateUpdate(endState, ressources);
 
         SetTextAlpha(0f);
 
         StartCoroutine(PlaySequenceCoroutine());
     }
 
-    private void SetupText(EEndRunState state)
+    private void StateUpdate(EEndRunState state, PlayerRessources ressources)
     {
         switch (state)
         {
             case EEndRunState.Success:
                 GameOverText.text = "YOUPI";
                 GameOverText.color = Color.yellow;
-                KeepPersistantRessources();
+                KeepPersistantRessources(ressources);
                 break;
 
             case EEndRunState.Death:
                 GameOverText.text = "YOU DIED (nulos)";
                 GameOverText.color = Color.red;
-                KeepPersistantRessources(); // todo remove here
+                KeepPersistantRessources(ressources); // todo remove here
                 break;
 
             case EEndRunState.Timeout:
                 GameOverText.text = "TIME OUT";
                 GameOverText.color = Color.gray;
-                KeepPersistantRessources();
+                KeepPersistantRessources(ressources);
                 break;
 
             default:
@@ -56,12 +57,14 @@ public class GameOverUIController : MonoBehaviour
         }
     }
 
-    private void KeepPersistantRessources()
+    private void KeepPersistantRessources(PlayerRessources ressources)
     {
-        var ressources = SaveManager.GetCurrentSaveAs<Save>().ressources;
+        var saveRessources = SaveManager.GetCurrentSaveAs<Save>().ressources;
         
-        // todo increment reel value
-        ressources.chromeCore += 1;
+        saveRessources.chromeCore += ressources.Ressources[(int)ERessourceType.ChromeCore];
+        saveRessources.eatherDust += ressources.Ressources[(int)ERessourceType.AetherDust];
+        saveRessources.starSingularity += ressources.Ressources[(int)ERessourceType.StarSingularity];
+        saveRessources.voidCrystal += ressources.Ressources[(int)ERessourceType.VoidCrystal];
 
         SaveManager.ManualSave();
     }
