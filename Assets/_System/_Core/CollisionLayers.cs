5193a028d8b37889188193cf2b7806ee4728a569
Athyrr <adamadhar28@gmail.com>
2026-04-29 17:11:48 +0200

    + Update Tags

--- Assets/_System/_Core/CollisionLayers.cs
+++ Assets/_System/_Core/CollisionLayers.cs
@@ -1,10 +1,10 @@
 public static class CollisionLayers
 {
     public const uint Player = 1 << 0;
+    
     public const uint Enemy = 1 << 1;
 
-    public const uint PlayerSpell = 1 << 2;
-    public const uint EnemySpell = 1 << 3;
+    public const uint Spell = 1 << 2;
 
     public const uint Obstacle = 1 << 4;
 
